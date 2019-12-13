7b3c1b0474c59a238f48988c7c46ff9240f9cae4
bbottema <b.bottema@projectnibble.org>
2019-12-13 16:44:12 +0100

    wip

---
+++ modules/module-b/src/main/java/com/github/bbottema/MainB.java
@@ -0,0 +1,15 @@
+package com.github.bbottema;
+
+/**
+ * Javadoc for module A
+ */
+public class MainB {
+	/**
+	 * Public method of MainB
+	 */
+	public void publicMethod(){}
+	/**
+	 * Private method of MainB
+	 */
+	private void privateMethod(){}
+}
